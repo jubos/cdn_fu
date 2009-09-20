fe7d0cc7042dbfcdc85b8b06080f8a2334151560
Curtis Spencer <thorin@gmail.com>
2009-09-20 12:23:37 -0700

    Changed some of the api methods to verbs for block related actions, and used nouns for setting of classes for things like minifier,lister, and uploader

--- init.rb
+++ init.rb
@@ -4,12 +4,6 @@ require 'fileutils'
 require 'stringio'
 require 'zlib'
 
-
-#Dir[File.join(File.dirname(__FILE__),"lib","*.rb")].each {|f| require f}
-["minifiers","listers","uploaders"].each do |subdir|
-  Dir[File.join(File.dirname(__FILE__),"lib",subdir, "*")].each {|f| require f}
-end
-
 ActionController::Base.class_eval do
   cattr_accessor :cdn_fu_config
 end
--- lib/cdn_fu_config.rb
+++ lib/cdn_fu_config.rb
@@ -1,8 +1,21 @@
 #Since CdnFuConfig is the entry point to the library from rails, make sure we
 #have everything else required here
-Dir[File.join(File.dirname(__FILE__),"*.rb")].each {|f| require f}
+Dir[File.join(File.dirname(__FILE__),"*.rb")].each {|f| require f if f !~ /cdn_fu_config.rb/}
 ["minifiers","listers","uploaders"].each do |subdir|
   Dir[File.join(File.dirname(__FILE__),subdir, "*")].each {|f| require f}
+end
+
+class Object
+  def cdn_metaclass
+    class << self
+      self
+    end
+  end
+
+  def cdn_meta_eval(&block)
+    puts "Evaling in #{self.class}"
+    cdn_metaclass.class_eval(&block)
+  end
 end
 
 # This is just a storage class for the configuration for CdnFu
@@ -51,19 +64,20 @@ class CdnFuConfig
 
   # Do some metaprogramming here for setting up different behaviors
   class << self
-    def preprocessor
+    def preprocess
       if block_given?
         @preprocessor = proc
       else 
-        raise CdnFuConfigError,"No Block Given"
+        raise CdnFuConfigError,"No preprocess block given"
       end
     end
 
-    def preprocessor=(preprocessor_class)
-      @preprocessor = preprocessor_class
+    def preprocessor(klass)
+      @preprocessor = klass
+      yield @preprocessor if block_given?
     end
 
-    def lister
+    def list
       if block_given?
         @lister = proc
       else
@@ -75,46 +89,30 @@ class CdnFuConfig
       @lister = lister_class
     end
 
-    # If a class is supplied, then the block is used for further configuration
-    # of the minifier
-    def minifier(klass = nil)
-      if klass
-        if block_given?
-          @minifier = klass
-          yield @minifier
-        end
+    def minify
+      if block_given?
+        @minifier = proc
       else
-        if block_given?
-          @minifier = proc
-        else
-          raise CdnFuConfigError,"No Block Given for Minifier"
-        end
+        raise CdnFuConfigError,"No Block Given for Minifier"
       end
     end
 
-    def minifier=(minifier_class)
+    def minifier(minifier_class)
       @minifier = minifier_class
+      yield @minifier if block_given?
     end
 
-    # If a class is supplied, then the block is used for further configuration
-    # of the uploader
-    def uploader(klass = nil)
-      if klass
-        if block_given?
-          @uploader = klass
-          yield @uploader
-        end
+    def upload
+      if block_given?
+        @uploader = proc
       else
-        if block_given?
-          @uploader = proc
-        else
-          raise CdnFuConfigError,"No Block Given for Uploader" 
-        end
+        raise CdnFuConfigError,"No Block Given"
       end
     end
 
-    def uploader=(uploader_class)
+    def uploader(uploader_class)
       @uploader = uploader_class
+      yield @uploader if block_given?
     end
   end
 end
